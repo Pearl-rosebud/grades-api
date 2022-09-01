f7b9d08891099857a086572780c9be2b733e5659
Pearl-rosebud <rosebudpearl37@gmail.com>
2022-09-01 07:09:56 -0700

    express app

---
+++ results.js
@@ -0,0 +1,11 @@
+const results = [{name:"Pearl Rosebud", class:"form three", grade: "8"},
+              {name:"Comport Yelfaare", class:"form three", grade: "12"},
+              {name:"Peter Yenko", class:"form three", grade: "30"},
+              {name:"Adinatu Adams", class:"form three", grade: "13"},
+              {name:"Atif Adamu", class:"form three", grade: "40"},
+              {name:"John Harry", class:"form three", grade: "10"},
+
+
+
+]
+module.exports = results;
